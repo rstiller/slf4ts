84100ef72000b88a40d591105eb5747fc1168568
rstiller <rstiller@users.noreply.github.com>
2017-07-14 03:48:02 +0200

    added pseudo test ...

--- packages/slf4ts-console/lib/slf4ts/index.ts
+++ packages/slf4ts-console/lib/slf4ts/index.ts
@@ -5,15 +5,42 @@ import {
     LogLevel,
 } from "slf4ts-api";
 
-class ConsoleLoggerImplementation implements LoggerImplementation {
+export class ConsoleLoggerImplementation implements LoggerImplementation {
 
     public async log(level: LogLevel, group: string, name: string, message: string, error: Error, metadata: any): Promise<any> {
-        throw new Error("Method not implemented.");
+        // tslint:disable:no-console
+        const timestamp = new Date();
+        let loggerName = "";
+
+        if (group && name) {
+            loggerName = `${group}.${name}`;
+        } else if (group) {
+            loggerName = group;
+        } else if (name) {
+            loggerName = name;
+        } else {
+            loggerName = "ROOT";
+        }
+
+        const logLevelName = `${LogLevel[level]}`;
+        const logMethodName = logLevelName.toLowerCase();
+        const msg = `${timestamp} [${loggerName}] ${logLevelName} - ${message}`;
+
+        let logMethod: (...args: any[]) => void = console.log;
+        if (logMethodName in console && (console as any)[logMethodName] instanceof Function) {
+            logMethod = (console as any)[logMethodName];
+        }
+
+        if (error) {
+            logMethod(msg, error, metadata);
+        } else {
+            logMethod(msg, metadata);
+        }
     }
 
 }
 
-class ConsoleLoggerBinding implements LoggerBinding {
+export class ConsoleLoggerBinding implements LoggerBinding {
 
     public getLoggerImplementation(): LoggerImplementation {
         return new ConsoleLoggerImplementation();
